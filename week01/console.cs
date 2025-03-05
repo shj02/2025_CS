5395ab8aaf96581532ccb882afe2184c24d7ad44
shj02 <163235571+shj02@users.noreply.github.com>
2025-03-05 19:33:36 +0900

    Console

---
+++ week01/console.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace FirstApp
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
