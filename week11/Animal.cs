1001b042787a6e0a5a94e95727dd0f609941dd74
shj0218 <163235571+shj02@users.noreply.github.com>
2025-05-16 16:17:27 +0900

    Create Animal.cs

---
+++ week11/Animal.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooApp {
+    abstract class Animal { //추상 클래스
+        private string _name;
+        protected int _level;
+        protected COLOR _color;
+
+        public string Name { get { return _name; } }
+        public COLOR Color { get { return _color; } }
+        public int Level { get { return _level; } }
+
+        public Animal(string name, COLOR color) {
+            _name = name;
+            _color = color;
+        }
+
+        public override string ToString() {
+            return $"ANIMAL: {_name}";
+        }
+
+        public virtual void Eat() { AddLevel(3); }
+
+        public void Play() { AddLevel(2); }
+
+        abstract protected bool AddLevel(int level); //추상메소드는 머리(header)만 있고 몸(내용)은 없음!
+    }
+}
