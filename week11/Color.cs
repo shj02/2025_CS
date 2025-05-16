2f4294d5ac875c157c42789a7e23e88061afe9b6
shj0218 <163235571+shj02@users.noreply.github.com>
2025-05-16 16:18:43 +0900

    Create Color.cs

---
+++ week11/Color.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooApp
+{
+    enum COLOR //열거형 enum (ctrl shift u : 대문자 변경, ctrl u : 소문자 변경)
+    {
+        WHITE,
+        BLACK,
+        YELLOW,
+        MIXED,
+
+        END,
+    }
+}
