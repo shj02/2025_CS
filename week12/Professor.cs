e8d1cd7a7a01f61649f6cede0328d03710b65feb
shj0218 <163235571+shj02@users.noreply.github.com>
2025-05-25 22:44:30 +0900

    Update Professor.cs

--- week12/Professor.cs
+++ week12/Professor.cs
@@ -6,33 +6,33 @@ using System.Threading.Tasks;
 
 namespace Week09Homework
 {
-    class Professor
+    class Professor : Member
     {
-        public string DepartmentCode { get; set; }
+        //public string DepartmentCode { get; set; }
 
-        private string _number;
-        public string Number
-        {
-            get { return _number; }
-        }
+        //private string _number;
+        //public string Number
+        //{
+        //    get { return _number; }
+        //}
 
-        private string _name;
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
+        //private string _name;
+        //public string Name
+        //{
+        //    get { return _name; }
+        //    set { _name = value; }
+        //}
 
         public override string ToString()
         {
             return $"[{Number}]{Name}";
         }
 
-        public Professor(string number, string name, string deptcode)
+        public Professor(string number, string name, Department dept) : base(number, name, dept)
         {
-            _number = number;
-            _name = name;
-            DepartmentCode = deptcode;
+            //_number = number;
+            //_name = name;
+            //DepartmentCode = deptcode;
         }
     }
 }
